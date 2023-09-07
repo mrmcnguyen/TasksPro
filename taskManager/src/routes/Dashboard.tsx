fd1ac11cd8dc321d7aab36675798ce9be0bb5bb2
Kevin Lee <hlee6997@uni.sydney.edu.au>
2023-09-07 23:22:14 +1000

    Trial #5

--- taskManager/src/routes/Dashboard.tsx
+++ taskManager/src/routes/Dashboard.tsx
@@ -1,70 +1,80 @@
-// TaskManager.tsx
 import React, { useState } from 'react';
 import { Task } from './Task';
 import Logo from '../resources/logoICON.png'
+import './Dashboard.css'
+
 
 function Dashboard(){
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState<Task>({ id: 0, title: '', description: '', completed: false });
+const [tasks, setTasks] = useState<Task[]>([]);
+const [newTask, setNewTask] = useState<Task>({ id: 0, title: '', description: '', completed: false });
 
-  const handleAddTask = () => {
-    setTasks([...tasks, newTask]);
-    setNewTask({ id: newTask.id + 1, title: '', description: '', completed: false });
-  };
 
-  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setNewTask({ ...newTask, [name]: value });
-  };
-
-  const handleToggleComplete = (taskId: number) => {
-    const updatedTasks = tasks.map(task =>
-      task.id === taskId ? { ...task, completed: !task.completed } : task
-    );
-    setTasks(updatedTasks);
-  };
-
-  return (
-    <body>
-    <div className="container">
-      <img className="logo" src={Logo}></img>
-      <h1 className='welcome-text'>Task Manager Dashboard</h1>
-      <div className='inputs'>
-        <input
-          type="text"
-          name="title"
-          placeholder="Task title"
-          value={newTask.title}
-          onChange={handleTaskChange}
-        />
-        <textarea
-          name="description"
-          placeholder="Task description"
-          value={newTask.description}
-          onChange={handleTaskChange}
-        />
-        <button onClick={handleAddTask}>Add Task</button>
-      </div>
-      <div>
-        {tasks.map(task => (
-          <div key={task.id}>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={() => handleToggleComplete(task.id)}
-            />
-            <span>{task.title}</span>
-            <button>Delete</button>
-          </div>
-        ))}
-      </div>
-    </div>
-    <p className="version-display">
-      Version 1.0.0
-      </p>
-    </body>
-    
-  );
+const handleAddTask = () => {
+setTasks([...tasks, newTask]);
+setNewTask({ id: newTask.id + 1, title: '', description: '', completed: false });
 };
 
+
+const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+const { name, value } = e.target;
+setNewTask({ ...newTask, [name]: value });
+};
+
+
+const handleToggleComplete = (taskId: number) => {
+const updatedTasks = tasks.map(task =>
+task.id === taskId ? { ...task, completed: !task.completed } : task
+);
+setTasks(updatedTasks);
+};
+
+
+return (
+
+<div className="container">
+<h1 className='welcome-text'>Task Manager Dashboard</h1>
+<div className='inputs'>
+<input
+className="taskName"
+type="text"
+name="title"
+placeholder="Task title"
+value={newTask.title}
+onChange={handleTaskChange}
+/>
+<textarea
+className='taskDescription'
+font-style
+name="description"
+placeholder="Task description"
+value={newTask.description}
+onChange={handleTaskChange}
+/>
+<button className='addTask' onClick={handleAddTask}>Add Task</button>
+</div>
+<div className='tasks'>
+{tasks.map(task => (
+<div key={task.id}>
+<input
+type="checkbox"
+checked={task.completed}
+onChange={() => handleToggleComplete(task.id)}
+/>
+<span>{task.title}</span>
+<button>Delete</button>
+</div>
+))}
+</div>
+<p className="version-display">
+Version 1.0.0
+</p>
+</div>
+
+);
+};
+
+
 export default Dashboard;
+
+
+
--- taskManager/src/routes/welcomeScreen.tsx
+++ taskManager/src/routes/welcomeScreen.tsx
@@ -6,28 +6,36 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import { Outlet } from "react-router-dom";
 
+
 function App() {
 
-  return (
-    <div className="App">
-      <div className='circle'></div>
-      <div>
-          <img src={tasksLogo} className="logo" alt="Tasks logo" />
-      </div>
-      <h1>Welcome to Tasks Pro.</h1>
-      <div className="card">
-        <Link to="/home">
-        <button className="get-started-button">
-          Get Started
-        </button>
-        </Link>
+return (
+<body className='screen'>
+<div className="App">
+<div className='circle'></div>
+<div>
+<img src={tasksLogo} className="logo" alt="Tasks logo"/>
+</div>
+<h1>Welcome to Tasks Pro.</h1>
+<div className="card">
+<Link to="/home">
+<button className="get-started-button">
+Get Started
+</button>
+</Link>
 
-      </div>
-      <p className="company-name-display">
-      Tasks Pro. 2023 ©. Powered by YMD. All rights reserved.
-      </p>
-    </div>
-  )
+
+</div>
+<p className="company-name-display">
+Tasks Pro. 2023 ©. Powered by YMD. All rights reserved.
+</p>
+</div>
+</body>
+)
 }
 
+
 export default App
+
+
+
